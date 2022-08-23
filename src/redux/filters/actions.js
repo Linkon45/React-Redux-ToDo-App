9ef59b07d98af28ddc00b43008844c7fd865d375
Linkon45 <linkon3.1416@gmail.com>
2022-08-23 21:51:35 +0600

    add filters actions

---
+++ src/redux/filters/actions.js
@@ -0,0 +1,18 @@
+import { STATUSCHANGED, COLORCHANGED } from "./actionTypes";
+
+export const colorChanged = (color, changeType) => {
+  return {
+    type: COLORCHANGED,
+    payload: {
+      color,
+      changeType,
+    },
+  };
+};
+
+export const statusChanged = (status) => {
+  return {
+    type: STATUSCHANGED,
+    payload: status,
+  };
+};
